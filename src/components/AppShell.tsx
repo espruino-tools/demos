17fc9c17c899ee0f7e122bcec8d3d41360e8293b
Callum McLuskey <callummcluskey100@gmail.com>
2022-10-10 19:41:44 +0100

    fixed footer

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -1,4 +1,5 @@
-import { Anchor, Breadcrumbs, Container, Divider, Footer, Group, Header, Title } from "@mantine/core";
+import { Anchor, Breadcrumbs, Center, Container, Divider, Footer, Text, Header, Title } from "@mantine/core";
+import { FaCopyright, FaRegCopyright } from "react-icons/fa";
 
 interface AppShellProps{
     children: React.ReactNode;
@@ -17,7 +18,7 @@ export const AppShell = ({children}:AppShellProps) => {
 
     return (
         <>
-        <div style={{minHeight:"calc(100vh - 270px)"}}>
+        <div style={{minHeight:"calc(100vh - 50px)"}}>
             <Header style={{backgroundColor:"white",display:'flex',alignItems:'center', gap: 30}} height={75} p="xs">
                 <Title pl="sm" order={2}>Espruino Demos</Title> 
                 <Breadcrumbs>{items}</Breadcrumbs>  
@@ -25,8 +26,10 @@ export const AppShell = ({children}:AppShellProps) => {
 
             {children}
             </div>
-            <Footer height={270} style={{background:'#2b3137'}}>
-
+            <Footer height={50} style={{background:'#2b3137'}}>
+                <Center style={{lineHeight:50, height:"100%"}}>
+                <FaRegCopyright color="white"/> <Text pl="sm" color={'white'}>Callum Mcluskey 2022</Text>
+                </Center>
             </Footer>
         </>
     )
