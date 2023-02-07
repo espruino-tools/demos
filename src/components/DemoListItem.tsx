cf93a8e04e48d39f742ee873af92796283829dda
Callum McLuskey <callummcluskey100@gmail.com>
2023-02-07 17:10:15 +0000

    changed magring

--- src/components/DemoListItem.tsx
+++ src/components/DemoListItem.tsx
@@ -1,7 +1,7 @@
 import { Card, Title, Text } from "@mantine/core"
 
 export const DemoListItem = ({thumbnail,name,description,link}:{thumbnail:string,name:string,link:string,description?:string}) => (
-    <Card withBorder mt="xl" mb="xl" sx={{maxWidth:400, minWidth:350}}>
+    <Card m='xl' withBorder sx={{maxWidth:400, minWidth:350}}>
         
         <a className="hdn" key={name} href={link}>
 
